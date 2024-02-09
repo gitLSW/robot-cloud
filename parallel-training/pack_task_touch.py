9f34b969bbd91ba1e53a34b4635b1611ac74e819
Laurenz W <no.reply.to.your.spam@com>
2024-02-09 12:31:42 +0100

    Changed stuff

--- parallel-training/pack_task_touch.py
+++ parallel-training/pack_task_touch.py
@@ -30,6 +30,8 @@ import omni.isaac.core.utils.numpy.rotations as rot_utils
 from omni.isaac.core.utils.rotations import lookat_to_quatf, gf_quat_to_np_array
 from omni.physx.scripts.utils import setRigidBody, setStaticCollider, setCollider, addCollisionGroup
 from scipy.spatial.transform import Rotation as R
+from pyquaternion import Quaternion
+
 
 # MESH_APPROXIMATIONS = {
 #         "none": PhysxSchema.PhysxTriangleMeshCollisionAPI,
@@ -48,13 +50,13 @@ LEARNING_STARTS = 10
 ENV_PATH = "World/Env"
 
 ROBOT_PATH = 'World/UR10e'
-ROBOT_POS = np.array([0.0, 0.0, 0.0])
+ROBOT_POS = np.array([0.0, 0.0, 0.2])
 
 LIGHT_PATH = 'World/Light'
 LIGHT_OFFSET = np.array([0, 0, 2])
 
 DEST_BOX_PATH = "World/DestinationBox"
-DEST_BOX_POS = np.array([0, -0.65, 0])
+DEST_BOX_POS = np.array([0, -0.65, 0.2])
 
 PART_PATH = 'World/Part'
 PART_HEIGHT = 0.6
@@ -97,14 +99,14 @@ class PackTask(BaseTask):
         self.max_steps = max_steps
 
         self.observation_space = spaces.Dict({
-            'gripper_state': spaces.Box(low=-3, high=3, shape=(8,)),
+            'gripper_closed': spaces.Discrete(2),
             # 'forces': spaces.Box(low=-1, high=1, shape=(8, 6)), # Forces on the Joints
             'box_state': spaces.Box(low=-3, high=3, shape=(NUMBER_PARTS, 2)), # Pos and Rot Distance of each part currently placed in Box compared to currently gripped part
-            'part_state': spaces.Box(low=-3, high=3, shape=(7,))
+            'part_state': spaces.Box(low=-3, high=3, shape=(6,))
         })
 
-        # End Effector Pose 
-        self.action_space = spaces.Box(low=-1, high=1, shape=(8,), dtype=float) # Delta Gripper Pose & gripper open / close
+        # End Effector Pose
+        self.action_space = spaces.Box(low=-1, high=1, shape=(7,), dtype=float) # Delta Gripper Pose & gripper open / close
 
         # trigger __init__ of parent class
         BaseTask.__init__(self, name=name, offset=offset)
@@ -184,66 +186,96 @@ class PackTask(BaseTask):
         default_pose = np.array([math.pi / 2, -math.pi / 2, -math.pi / 2, -math.pi / 2, math.pi / 2, 0])
         self.robot.set_joint_positions(positions=default_pose)
 
-        gripper_pos, gripper_rot = self.robot.gripper.get_world_pose()
-        self.robot.gripper.open()
+        # gripper_pos, gripper_rot = self.robot.gripper.get_world_pose()
+        # gripper_pos -= self.robot.get_world_pose()[0]
+        # self.robot.gripper.open()
+        #  np.concatenate((gripper_pos, gripper_rot, [-1]), axis=0)
 
-        worst_empty_box = np.full((NUMBER_PARTS, 2), [3, math.pi])
+
+        box_state, current_ideal_pose = self.compute_box_state()
+
+        part_pos, part_rot = self.part.get_world_pose()
+        part_pos -= self.box.get_world_pose()[0]
+        part_rot_euler = R.from_quat(part_rot).as_euler('xyz',degrees=False)
+        ideal_rot_euler = R.from_quat(current_ideal_pose[1]).as_euler('xyz',degrees=False)
+        part_pos_diff = current_ideal_pose[0] - part_pos
+        part_rot_diff = ideal_rot_euler - part_rot_euler
 
         return {
-            'gripper_state': np.concatenate((gripper_pos, gripper_rot, [-1]), axis=0),
-            'box_state': worst_empty_box,
-            'part_state': np.concatenate(self.part.get_world_pose(), axis=0)
+            'gripper_closed': False,
+            'box_state': box_state,
+            'part_state': np.concatenate((part_pos_diff, part_rot_diff), axis=0)
         }
 
 
     placed_parts = []
     def get_observations(self):
-        gripper = self.robot.gripper
-        gripper_pose_kin = self.kinematics_solver.compute_end_effector_pose()
-        gripper_pos, gripper_rot = gripper.get_world_pose()
-        gripper_pos -= self.robot.get_world_pose()[0]
-        gripper_closed = 2 * float(gripper.is_closed()) - 1
+        # gripper = self.robot.gripper
+        # gripper_pos, gripper_rot = gripper.get_world_pose()
+        # gripper_pos -= self.robot.get_world_pose()[0]
 
-        # TODO: Was sollen wir verwenden ??!!
-        print('COMPARE:')
-        print(gripper_pose_kin)
-        print((gripper_pos, gripper_rot))
+        # gripper_pose_kin = self.kinematics_solver.compute_end_effector_pose()
+        # gripper_closed = 2 * float(gripper.is_closed()) - 1
+        # # 'gripper_state': np.concatenate((gripper_pos, gripper_rot, [gripper_closed]), axis=0),
 
-        # A 2D Array where each entry is the poses of the parts in the box
-        part_pos, part_rot= self.part.get_world_pose()
-        part_pos -= self.box.get_world_pose()[0]
-        box_state = []
-        selection = IDEAL_PACKAGING
-        for i in range(NUMBER_PARTS):
-            if len(self.placed_parts) <= i:
-                box_state.append([3, math.pi])
-                continue
-            part = self.placed_parts[i]
-            ideal_rot = None
-            ideal_part_index = None
-            min_dist = 10000000
-            for index, part in enumerate(selection):
-                dist = np.linalg.norm(part[0] - part_pos)
-                if dist < min_dist:
-                    ideal_rot = part[1]
-                    ideal_part_index = index
-                    min_dist = dist
-            selection.remove(ideal_part_index)
-            rot_dist = self._shortest_rot_dist(part_rot, ideal_rot)
-            box_state.append([min_dist, rot_dist])
+        # # TODO: Was sollen wir verwenden ??!!
+        # print('COMPARE:')
+        # print(gripper_pose_kin)
+        # print((gripper_pos, gripper_rot))
 
         # forces = self.robot.get_measured_joint_forces()
+
+        box_state, current_ideal_pose = self.compute_box_state()
+
+        part_pos, part_rot = self.part.get_world_pose()
+        part_pos -= self.box.get_world_pose()[0]
+        part_rot_euler = R.from_quat(part_rot).as_euler('xyz',degrees=False)
+        ideal_rot_euler = R.from_quat(current_ideal_pose[1]).as_euler('xyz',degrees=False)
+        part_pos_diff = current_ideal_pose[0] - part_pos
+        part_rot_diff = ideal_rot_euler - part_rot_euler
+
         return {
-            'gripper_state': np.concatenate((gripper_pos, gripper_rot, [gripper_closed]), axis=0),
+            'gripper_closed': self.robot.gripper.is_closed(),
             'box_state': box_state,
             # 'forces': forces,
-            'part_state':  np.concatenate((part_pos, part_rot), axis=0)
+            'part_state':  np.concatenate((part_pos_diff, part_rot_diff), axis=0)
         }
 
 
+    # Returns: A 2D Array where each entry is the poses of the parts in the box
+    def compute_box_state(self):
+        box_state = []
+        ideal_selection = IDEAL_PACKAGING.copy()
+        parts = self.placed_parts + [self.part]
+        current_ideal_pose = None
+
+        for i in range(NUMBER_PARTS):
+            if len(parts) <= i:
+                box_state.append([3, math.pi])
+                continue
+            part = parts[i]
+            part_pos, part_rot= part.get_world_pose()
+            part_pos -= self.box.get_world_pose()[0]
+
+            ideal_part = None
+            min_dist = 10000000
+            # Find closest ideal part
+            for sel_part in ideal_selection:
+                dist = np.linalg.norm(sel_part[0] - part_pos)
+                if dist < min_dist:
+                    ideal_part = sel_part
+                    min_dist = dist
+                    if i == len(parts) - 1:
+                        current_ideal_pose = ideal_part
+
+            ideal_selection.remove(ideal_part)
+            rot_dist = _shortest_rot_dist(part_rot, ideal_part[1])
+            box_state.append([min_dist, rot_dist])
+
+        return box_state, current_ideal_pose
+
 
     def pre_physics_step(self, actions) -> None:
-        self.step += 1
         gripper = self.robot.gripper
         
         if self.step == LEARNING_STARTS -1:
@@ -257,9 +289,10 @@ class PackTask(BaseTask):
         
         # Rotate Joints
         gripper_pos = actions[0:3]
-        gripper_rot = actions[3:7]
-        gripper_action = actions[7]
+        gripper_rot_euler = actions[3:6]
+        gripper_action = actions[6]
 
+        gripper_rot = R.from_euler('xyz', gripper_rot, degrees=False).as_quat()
         movement, success = self.kinematics_solver.compute_inverse_kinematics(gripper_pos, gripper_rot)
         print('success', success)
         if success:
@@ -276,7 +309,20 @@ class PackTask(BaseTask):
     # Calculate Rewards
     step = 0
     def calculate_metrics(self) -> None:
-        return 0, False
+        self.step += 1
+        # Terminate: Umgefallene Teile, Gefallene Teile
+        # Success: 
+        part_pos, part_rot = self.part.get_world_pose()
+
+        if part_pos[2] < 0.2 or self.max_steps < self.step:
+            return -MAX_STEP_PUNISHMENT, True
+
+        box_state, _ = self.compute_box_state()
+        box_deviation = np.sum(np.square(box_state))
+
+        # placed_parts.append(self.part)
+
+        return -box_deviation, False
         # gripper_pos = self.robot.gripper.get_world_pose()[0]
 
         # self.step += 1
@@ -315,9 +361,6 @@ class PackTask(BaseTask):
 
 
 def _shortest_rot_dist(quat_1, quat_2):
-        quat_1 = R.from_quat(quat_1)
-        quat_2 = R.from_quat(quat_2)
-        if (np.dot(quat_1, quat_2) < 0):
-            return quat_1 * R.inv(-quat_2)
-        else:
-            return quat_1 * R.inv(quat_2)
+    part_quat = Quaternion(quat_1)
+    ideal_quat = Quaternion(quat_2)
+    return Quaternion.absolute_distance(part_quat, ideal_quat)
